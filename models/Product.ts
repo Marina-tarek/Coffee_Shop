26cd7a7916c7c0e120b12edd66ea8e15d3802dd0
Marina-tarek <marina.tarek.w@gmail.com>
2025-10-16 20:42:56 +0300

    first commit

---
+++ models/Product.ts
@@ -0,0 +1,13 @@
+import { Schema, model, models } from "mongoose";
+
+const productSchema = new Schema({
+  name: { type: String, required: true },
+  slug: { type: String, unique: true },
+  description: String,
+  price: { type: Number, required: true },
+  image: String,
+  category: String,
+  createdAt: { type: Date, default: Date.now },
+});
+
+export default models.Product || model("Product", productSchema);
