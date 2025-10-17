8c448bc9473b45705e2b0dbb492b25d17f8f8742
Marina-tarek <marina.tarek.w@gmail.com>
2025-10-17 18:11:52 +0300

    small edit

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,10 +1,17 @@
 import Link from "next/link";
 
 async function getProducts() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/products`, {
-    cache: "no-store",
-  });
-  return res.json();
+const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/products`,{
+cache: "no-store",
+});
+
+if (!res.ok) {
+  const text = await res.text();
+  throw new Error(`Failed to fetch products: ${text}`);
+}
+
+const data = await res.json();
+return data;
 }
 
 export default async function ProductsPage() {
